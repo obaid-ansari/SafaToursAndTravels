7b77c04e8c14e4d46262c97442f3d8bc5d224483
Obaid Ansari <ansari.ubaid.1020@gmail.com>
2026-06-30 20:06:25 +0530

    setup for gh-pages

--- client/src/services/form.service.jsx
+++ client/src/services/form.service.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://safatoursandtravels-ypf9.onrender.com/api",
+  baseURL: "https://safatoursandtravels.vercel.app/api",
   withCredentials: true,
 });
 
